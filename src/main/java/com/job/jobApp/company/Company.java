46f1133feef700c51ef7897382e85e4144b9a374
rakesh1039 <rakesh1039@gmail.com>
2025-06-28 02:11:54 +0530

    modify on company class

--- src/main/java/com/job/jobApp/company/Company.java
+++ src/main/java/com/job/jobApp/company/Company.java
@@ -20,41 +20,48 @@ public class Company {
     @OneToMany(mappedBy = "company")
     private List<Job> jobs;
 
-    @JsonIgnore
     @OneToMany(mappedBy = "company")
     private List<Review> reviews;
 
     public Company() {
     }
 
-    public Company(Long id, String name, String description, List<Job> jobs) {
+    public Company(Long id, String name, String description, List<Job> jobs, List<Review> reviews) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.jobs = jobs;
+        this.reviews = reviews;
     }
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public List<Job> getJobs() {
         return jobs;
     }
+
     public void setJobs(List<Job> jobs) {
         this.jobs = jobs;
     }
